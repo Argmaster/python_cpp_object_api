615a622d50bf9752bf0c95504455abac0be3577c
Argmaster <argmaster.world@gmail.com>
2021-07-05 00:53:37 +0200

    Added Py::Long value retrival and creation methods

--- source/Long.cpp
+++ source/Long.cpp
@@ -3,7 +3,13 @@
 
 namespace Py
 {
-    Long::Long(long _long) : Object(nullptr) {
-        m_ref = PyLong_FromLong(_long);
+    Long::Long(int64_t _long) : Object(nullptr) {
+        m_ref = PyLong_FromLongLong(_long);
+    }
+    Long(std::string _value, int base = 10) {
+        m_ref = PyLong_FromString(_value.begin(), _value.end(), base);
+    }
+    Long::Long(Py::Str _value, int base) {
+        m_ref = PyLong_FromUnicodeObject(_value, base);
     }
 } // namespace Py
--- test/i_Core.cpp
+++ test/i_Core.cpp
@@ -3,7 +3,6 @@
 int main() {
     Py::Initialize();
     try {
-
         Py::Modules.traceback->Call("print", { Py::Str("Hello world") }, nullptr);
     } catch (Py::Exception::Error err) {
         Py::print(err.message);
