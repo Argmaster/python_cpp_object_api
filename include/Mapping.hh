a2e709db1fababca1f2f45d846beefc212ca88f6
Argmaster <argmaster.world@gmail.com>
2021-07-06 17:48:40 +0200

    marked Mapping methods const

--- include/Mapping.hh
+++ include/Mapping.hh
@@ -13,19 +13,19 @@ namespace Py
             Return element of o corresponding to the string key or NULL on failure.
             This is the equivalent of the Python expression o[key]. See also PyObject_GetItem().
         */
-        Object  GetItem(const std::string& key);
+        Object  GetItem(const std::string& key) const;
         /*
             Map the string key to the value v in object o. Returns -1 on failure.
             This is the equivalent of the Python statement o[key] = v. See also PyObject_SetItem().
             This function does not steal a reference to v.
         */
-        int     SetItem(const std::string& key, Object value);
+        int     SetItem(const std::string& key, Object value) const;
         /*
             Remove the mapping for the object key from the object o.
             Return -1 on failure. This is equivalent to the Python
             statement del o[key]. This is an alias of PyObject_DelItem().
         */
-        int     DelItem(const std::string& key);
+        int     DelItem(const std::string& key) const;
         /*
             Return 1 if the mapping object has the key key and 0 otherwise.
             This is equivalent to the Python expression key in o.
@@ -34,7 +34,7 @@ namespace Py
             method will get suppressed. To get error reporting use
             PyObject_GetItem() instead.
         */
-        int     HasKey(const std::string& key);
+        int     HasKey(const std::string& key) const;
         /*
             Return a List containing all the items from the mapping.
         */
--- source/Mapping.cpp
+++ source/Mapping.cpp
@@ -9,16 +9,16 @@ namespace Py
             PyMapping_SetItemString(m_ref, pair.first.c_str(), pair.second);
         }
     }
-    Object  Mapping::GetItem(const std::string& key) {
+    Object  Mapping::GetItem(const std::string& key) const {
         return New<Object>(PyMapping_GetItemString(m_ref, key.c_str()));
     }
-    int     Mapping::SetItem(const std::string& key, Object value) {
+    int     Mapping::SetItem(const std::string& key, Object value) const {
         return PyMapping_SetItemString(m_ref, key.c_str(), value);
     }
-    int     Mapping::DelItem(const std::string& key) {
+    int     Mapping::DelItem(const std::string& key) const {
         return PyMapping_DelItemString(m_ref, key.c_str());
     }
-    int     Mapping::HasKey(const std::string& key) {
+    int     Mapping::HasKey(const std::string& key) const {
         return PyMapping_HasKeyString(m_ref, key.c_str());
     }
     List    Mapping::Items() const {
