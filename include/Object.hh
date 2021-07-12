1125734edf41d81c65135f29fe3e5e1e46412648
Argmaster <argmaster.world@gmail.com>
2021-07-12 16:35:42 +0200

    distingushed instance method and method Is checks

--- include/Object.hh
+++ include/Object.hh
@@ -178,7 +178,8 @@ namespace Py
         inline bool         IsTime() { return PyTime_CheckExact(m_ref); }
         inline bool         IsDelta() { return PyDelta_CheckExact(m_ref); }
         inline bool         IsTZInfo() { return PyTZInfo_CheckExact(m_ref); }
-        inline bool         IsMethod() { return PyInstanceMethod_Check(m_ref); }
+        inline bool         IsInstanceMethod() { return PyInstanceMethod_Check(m_ref); }
+        inline bool         IsMethod() { return PyMethod_Check(m_ref); }
         /* -------------------------------------------------------------------------- */
         /*                          Getters, setter, deleters                         */
         /* -------------------------------------------------------------------------- */
